88b506b9825a0ba36bfe42f0c806f2841b6252dc
Carolyn Stransky <carolynstransky@gmail.com>
2020-04-29 19:19:22 +0200

    [MEM-418] Basic org routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Link from 'next/link';
+import {
+  Stack,
+  Text,
+  Grid,
+  Image,
+  useColorMode,
+  Heading,
+  Icon,
+  Link as ChakraLink
+} from "@chakra-ui/core";
+import Card from "../components/molecules/card";
+// import { repos } from "../data/repoQuery";
+import { GraphQLClient } from "graphql-request";
+
+const graphcms = new GraphQLClient(process.env.gcms);
+
+export async function getStaticProps() {
+  const { projects } = await graphcms.request(
+    `
+    query ByUser($user: String) {
+      projects(where: {user: $user}) {
+        tests {
+          branchName
+          failureMessage
+          id
+          testDate
+          testStatus
+          testType
+        }
+        user
+        organizationName
+        organizationImage {
+          handle
+        }
+        repositoryName
+      }
+    }
+  `,
+    {
+      user: "KenzoBenzo",
+    }
+  );
+
+  return {
+    props: {
+      projects,
+    },
+  };
+}
+
+export default function Home({ projects }) {
+  const { colorMode } = useColorMode();
+  console.log(projects);
+  return (
+    <>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {projects.map((project, index) => (
+          <Link key={project.organizationName} href={`/${project.organizationName}/`}>
+          <Card key={index}>
+            <Stack spacing={4} isInline>
+              <Image
+                size={10}
+                src={`https://media.graphcms.com/${project.organizationImage.handle}`}
+                bg="gray.50"
+                border="1px solid"
+                borderColor={`mode.${colorMode}.icon`}
+                rounded="sm"
+              />
+              <Stack spacing={2}>
+                <Text color={`mode.${colorMode}.text`} lineHeight="none">
+                  {project.organizationName}
+                </Text>
+                <Heading
+                  as="h3"
+                  lineHeight="none"
+                  fontSize="md"
+                  fontWeight={900}
+                >
+                  {project.repositoryName}
+                </Heading>
+              </Stack>
+            </Stack>
+          </Card>
+          </Link>
+        ))}
+        <ChakraLink
+          href="https://github.com/apps/meeshkan/installations/new"
+          bg={`mode.${colorMode}.card`}
+          p={4}
+          rounded="sm"
+          color={`mode.${colorMode}.title`}
+          _hover={{ color: `mode.${colorMode}.titleHover` }}
+        >
+          <Stack spacing={4} align="center" isInline>
+            <Icon h={10} w={10} name="add" stroke="2px" />
+            <Heading as="h3" lineHeight="none" fontSize="md" fontWeight={900}>
+              Authorize a repository
+            </Heading>
+          </Stack>
+        </ChakraLink>
+      </Grid>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link';
 import {
   Stack,
   Text,
@@ -7,10 +8,9 @@ import {
   useColorMode,
   Heading,
   Icon,
-  Link,
+  Link as ChakraLink
 } from "@chakra-ui/core";
 import Card from "../components/molecules/card";
-// import { repos } from "../data/repoQuery";
 import { GraphQLClient } from "graphql-request";
 
 const graphcms = new GraphQLClient(process.env.gcms);
@@ -49,83 +49,11 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ projects }) {
-  const { colorMode } = useColorMode();
-  console.log(projects);
-  return (
-    <>
-      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-        {projects.map((project, index) => (
-          <Card
-            link={`/${project.organizationName}-${project.repositoryName}/dashboard`}
-            key={index}
-          >
-            <Stack spacing={4} isInline>
-              <Image
-                size={10}
-                src={`https://media.graphcms.com/${project.organizationImage.handle}`}
-                bg="gray.50"
-                border="1px solid"
-                borderColor={`mode.${colorMode}.icon`}
-                rounded="sm"
-              />
-              <Stack spacing={2}>
-                <Text color={`mode.${colorMode}.text`} lineHeight="none">
-                  {project.organizationName}
-                </Text>
-                <Heading
-                  as="h3"
-                  lineHeight="none"
-                  fontSize="md"
-                  fontWeight={900}
-                >
-                  {project.repositoryName}
-                </Heading>
-              </Stack>
-            </Stack>
-          </Card>
-        ))}
-
-        {/* {repos.map((repo, index) => (
-          <Card
-            link={`/${repo.organization.toLowerCase()}-${repo.repository.toLowerCase()}/dashboard`}
-            key={index}
-          >
-            <Stack spacing={4} isInline>
-              <Image size={10} src={repo.image} rounded="sm" />
-              <Stack spacing={2}>
-                <Text color={`mode.${colorMode}.text`} lineHeight="none">
-                  {repo.organization.toLowerCase()}
-                </Text>
-                <Heading
-                  as="h3"
-                  lineHeight="none"
-                  fontSize="md"
-                  fontWeight={900}
-                >
-                  {repo.repository.toLowerCase()}
-                </Heading>
-              </Stack>
-            </Stack>
-          </Card>
-        ))} */}
-
-        <Link
-          href="https://github.com/apps/meeshkan/installations/new"
-          bg={`mode.${colorMode}.card`}
-          p={4}
-          rounded="sm"
-          color={`mode.${colorMode}.title`}
-          _hover={{ color: `mode.${colorMode}.titleHover` }}
-        >
-          <Stack spacing={4} align="center" isInline>
-            <Icon h={10} w={10} name="add" stroke="2px" />
-            <Heading as="h3" lineHeight="none" fontSize="md" fontWeight={900}>
-              Authorize a repository
-            </Heading>
-          </Stack>
-        </Link>
-      </Grid>
-    </>
-  );
-}
+export default ({ projects }) => 
+  projects.map(({ organizationName, repositoryName }) => {
+    return(
+      <Link key={organizationName} href={`/${organizationName}/`}>
+        <a>{organizationName}/{repositoryName}</a>
+      </Link>
+    );
+  })
