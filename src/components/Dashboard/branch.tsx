a65aca188681976a93fbf8b892cf51177d5772b1
Carolyn Stransky <carolynstransky@gmail.com>
2020-04-30 18:24:19 +0200

    [MEM-322] Moar log cleanup, moar types

--- src/components/Dashboard/branch.tsx
+++ src/components/Dashboard/branch.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Card from "../molecules/card";
 import { Test } from "../molecules/test";
 
-const Branch = ({ tests }) => {
-  console.log('Branch tests', tests)
+type BranchProps = {
+  tests: Array<any>;
+};
+
+const Branch = ({ tests }: BranchProps) => {
   return (
     <Card gridArea="1 / 3 / 3 / 4" heading="Branch tests" headingLink="/branch">
       {tests.map((test, index) => (
--- src/components/Dashboard/production.tsx
+++ src/components/Dashboard/production.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Card from "../molecules/card";
 import { Test } from "../molecules/test";
 
-const Production = ({ tests }) => {
-   console.log('Production tests', tests)
+type ProductionProps = {
+  tests: Array<any>;
+};
+
+const Production = ({ tests }: ProductionProps) => {
   return (
     <Card gridArea="1 / 2 / 2 / 3" heading="Production tests" headingLink="/production">
       {tests.map((test, index) => (
