d9802fd2aee39e0e8771812a34420a1df9d25dd6
Carolyn Stransky <carolynstransky@gmail.com>
2020-04-30 18:36:33 +0200

    [MEM-322] Fallback for when there are no tests to display

--- src/components/Dashboard/branch.tsx
+++ src/components/Dashboard/branch.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Card from "../molecules/card";
 import { Test } from "../molecules/test";
+import { Text, useColorMode } from "@chakra-ui/core";
 
 type BranchProps = {
   tests: Array<any>;
 };
 
 const Branch = ({ tests }: BranchProps) => {
+  const { colorMode } = useColorMode();
   return (
     <Card gridArea="1 / 3 / 3 / 4" heading="Branch tests" headingLink="/branch">
-      {tests.map((test, index) => (
+      { tests.length === 0 ?
+        <Text mt={2} color={`mode.${colorMode}.text`}>
+          You haven't done any branch tests yet.
+        </Text>
+      :
+      tests.map((test, index) => (
         <Test key={index} branchName={test.branchName} date={test.testDate} status={test.testStatus} />
-      ))}
+      ))
+      }
     </Card>
   );
 };
--- src/components/Dashboard/production.tsx
+++ src/components/Dashboard/production.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Card from "../molecules/card";
 import { Test } from "../molecules/test";
+import { Text, useColorMode } from "@chakra-ui/core";
 
 type ProductionProps = {
   tests: Array<any>;
 };
 
 const Production = ({ tests }: ProductionProps) => {
+  const { colorMode } = useColorMode();
   return (
     <Card gridArea="1 / 2 / 2 / 3" heading="Production tests" headingLink="/production">
-      {tests.map((test, index) => (
+      { tests.length === 0 ?
+        <Text mt={2} color={`mode.${colorMode}.text`}>
+          You haven't done any production tests yet.
+        </Text>
+      :
+      tests.map((test, index) => (
         <Test key={index} branchName={test.branchName} date={test.testDate} status={test.testStatus} />
-      ))}
+      ))
+      }
     </Card>
   );
 };
