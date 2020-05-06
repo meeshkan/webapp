126b2c74d80ef3beef3808580cadb63744a70b27
Mike Solomon <mike@meeshkan.com>
2020-05-06 12:48:39 +0300

    Moves logic to server-side only

--- src/pages/api/me.js
+++ src/pages/api/me.js
@@ -1,8 +1,56 @@
 import auth0 from '../../utils/auth0';
+import { GraphQLClient } from 'graphql-request';
 
 export default async function me(req, res) {
   try {
-    await auth0.handleProfile(req, res);
+    const { user } = await auth0.getSession(req);
+    // fetch the user on github
+    const ghLogin = user.nickname;
+
+    const ghGraphQLClient = new GraphQLClient('https://api.github.com/graphql', {
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_USER_INFO_AUTH_TOKEN}`,
+      },
+    });
+  
+    const ghData = await ghGraphQLClient.request(`query {
+      user(login: "${ghLogin}") {
+              id
+          }
+      }`);
+    user.node_id = ghData.user.id;
+
+    const gcmsGraphQLClient = new GraphQLClient('https://api.github.com/graphql', {
+      headers: {
+        authorization: `Bearer ${process.env.GRAPHCMS_QUERY_AUTH_TOKEN}`,
+      },
+    });
+
+    const gcmsData = await gcmsGraphQLClient.request(`query {
+      projects(where: {
+        user: { githubUserNodeId: "${user.node_id}" }
+      }) {
+        githubRepositoryNodeId
+        githubOrganizationNodeId
+        organizationImage {
+          fileName
+        }
+      }
+    }`);
+
+    const projectsQuery = gcmsData.projects.map((project, i) => `q${i}: repository(id: "${project.githubRepositoryNodeId}") {
+            owner {
+                login
+                avatarUrl
+            }
+            id
+            name
+        }`);
+    const projectsData = await ghGraphQLClient.request(`query {
+      ${projectsQuery.join('\n')}
+      }`);
+    user.projects = Object.values(projectsData);
+    res.json(user);
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
--- src/utils/user.js
+++ src/utils/user.js
@@ -1,6 +1,5 @@
 import React from "react";
 import fetch from "isomorphic-unfetch";
-import { GraphQLClient } from 'graphql-request';
 
 // Use a global to save the user, so we don't have to fetch it again after page navigations
 let userState;
@@ -14,54 +13,6 @@ export const fetchUser = async () => {
 
   const res = await fetch("/api/me");
   userState = res.ok ? await res.json() : null;
-  if (userState !== null) {
-    // fetch the user on github
-    const ghLogin = userState.nickname;
-
-    const ghGraphQLClient = new GraphQLClient('https://api.github.com/graphql', {
-      headers: {
-        authorization: `Bearer ${process.env.GITHUB_USER_INFO_AUTH_TOKEN}`,
-      },
-    });
-  
-    const ghData = await ghGraphQLClient.request(`query {
-      user(login: "${ghLogin}") {
-              id
-          }
-      }`);
-    userState.node_id = ghData.user.id;
-
-    const gcmsGraphQLClient = new GraphQLClient('https://api.github.com/graphql', {
-      headers: {
-        authorization: `Bearer ${process.env.GRAPHCMS_QUERY_AUTH_TOKEN}`,
-      },
-    });
-
-    const gcmsData = await gcmsGraphQLClient.request(`query {
-      projects(where: {
-        user: { githubUserNodeId: "${userState.node_id}" }
-      }) {
-        githubRepositoryNodeId
-        githubOrganizationNodeId
-        organizationImage {
-          fileName
-        }
-      }
-    }`);
-
-    const projectsQuery = gcmsData.projects.map((project, i) => `q${i}: repository(id: "${project.githubRepositoryNodeId}") {
-            owner {
-                login
-                avatarUrl
-            }
-            id
-            name
-        }`);
-    const projectsData = await ghGraphQLClient.request(`query {
-      ${projectsQuery.join('\n')}
-      }`);
-    userState.projects = Object.values(projectsData);
-  }
   return userState;
 };
 
