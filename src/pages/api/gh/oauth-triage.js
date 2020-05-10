9332cac5046a4437452acbdaddb4798373b7962e
Mike Solomon <mike@meeshkan.com>
2020-05-10 13:38:47 +0300

    Adds triage for inbound github oauth. Because github does not have multiple valid redirect urls, we need to have a single one deployed at all times. This can then do triage.
    Currently, we hardcode two triage outcomes - dev (which goes to localhost)
    and a default, which is production.

---
+++ src/pages/api/gh/oauth-triage.js
@@ -0,0 +1,14 @@
+// because github does not allow for multiple oauth
+// redirect urls, we have to create a triage where
+export default (req, res) => {
+    const {
+      query: { state, code },
+    } = req
+    const parsedState = JSON.parse(state);
+    const Location = (parsedState.env === 'dev' ? 'http://localhost:3000/api/gh/oauth' : 'https://api.meeshkan.com/api/gh/oauth')+'?state='+state+'&code='+code;
+    console.log("location", Location);
+    res.writeHead(301, {
+        Location
+    });
+    res.end();
+}
