07032788f9a3572b6bf0d5790f9f40c161bf549c
Mike Solomon <mike@meeshkan.com>
2020-05-20 04:58:15 +0300

    Sends logs to cloudflare from in app

--- src/utils/safeApi.ts
+++ src/utils/safeApi.ts
@@ -3,10 +3,13 @@ import { pipe } from "fp-ts/lib/pipeable";
 import { bracket, fromEither, right, TaskEither } from "fp-ts/lib/TaskEither";
 import { NextApiRequest, NextApiResponse } from "next";
 import logger from "pino";
+import fetch from "isomorphic-unfetch";
 import { INCORRECT_TYPE_SAFETY } from "./error";
 import { PathReporter } from "io-ts/lib/PathReporter";
 import * as E from "fp-ts/lib/Either";
 import * as t from "io-ts";
+import * as I from "fp-ts/lib/Identity";
+import { constNull } from "fp-ts/lib/function";
 
 const Logger = logger();
 
@@ -24,17 +27,29 @@ export const _400ErrorHandler = <E extends object>(
   _req: NextApiRequest,
   _res: NextApiResponse
 ) => (e: E) => {
-  Logger.error("Returning 400");
-  Logger.error(
+  pipe(
     incorrectTypeSafetyHack(e)
       ? {
           type: e.type,
           msg: e.msg,
           errors: PathReporter.report(E.left(e.errors)),
         }
-      : e
+      : e,
+    I.chainFirst((toLog) =>
+      process.env.LOGFLARE_PRODUCTION_URL
+        ? fetch(process.env.LOGFLARE_PRODUCTION_URL, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify([toLog]),
+          })
+        : constNull()
+    ),
+    I.chainFirst((toLog) => Logger.error(toLog)),
+    () => _res.status(400)
   );
-  _res.status(400);
 };
 
 export default <E>(
