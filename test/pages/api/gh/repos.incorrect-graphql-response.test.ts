f4bdcac3c3e0645be2f0ad0135d2fce7cd23c155
Mike Solomon <mike@meeshkan.com>
2020-05-20 12:00:49 +0300

    Abstracts out auth0 and mocks

--- test/pages/api/gh/repos.incorrect-graphql-response.test.ts
+++ test/pages/api/gh/repos.incorrect-graphql-response.test.ts
@@ -1,37 +1,14 @@
-///////// setup
-// auth0
-jest.mock("../../../../src/utils/auth0");
-const auth0 = require("../../../../src/utils/auth0");
-auth0.default.mockImplementation(() => ({
-  getSession: () =>
-    Promise.resolve({
-      idToken: "mock-id-token",
-      user: {
-        email: "mock@email.com",
-        picture: "https://mock-picture.io/me",
-      },
-    }),
-}));
-// graphql request with incorrect type safety
-jest.mock("graphql-request");
-const gqlr = require("graphql-request");
+import { mockAuth0WithSession } from "../../../../mocks/auth0";
+import { mockRequest, mockResponse } from "../../../../mocks/reqres";
+import { mock8BaseWithResult } from "../../../../mocks/8base";
 
-gqlr.GraphQLClient.mockImplementation(() => ({
-  // FORCE INCORRECT TYPE SAFETY with dummy response
-  request: () => Promise.resolve(0),
-}));
-
-// request and response
-import { NextApiRequest, NextApiResponse } from "next";
-const mockRequest = {} as NextApiRequest;
-// @ts-ignore
-const mockResponse = { status: jest.fn(), end: jest.fn() } as NextApiResponse;
-
-//////////////////// the test
 import endpoint from "../../../../src/pages/api/gh/repos";
 import * as E from "fp-ts/lib/Either";
 
+mockAuth0WithSession();
+mock8BaseWithResult(0);
+
 test("endpoint returns null without any additional mocking", () =>
   expect(
-    endpoint(mockRequest, mockResponse).then(E.mapLeft((e) => e.type))
+    endpoint(mockRequest(), mockResponse()).then(E.mapLeft((e) => e.type))
   ).resolves.toEqual(E.left("INCORRECT_TYPE_SAFETY")));
--- test/pages/api/gh/repos.no-session.test.ts
+++ test/pages/api/gh/repos.no-session.test.ts
@@ -1,22 +1,12 @@
-///////// setup
-// auth0
-jest.mock("../../../../src/utils/auth0");
-const auth0 = require("../../../../src/utils/auth0");
-auth0.default.mockImplementation(() => ({
-  getSession: () => Promise.resolve(null),
-}));
-
-////// request response
-
-const mockRequest = {} as NextApiRequest;
-// @ts-ignore
-const mockResponse = { status: jest.fn(), end: jest.fn() } as NextApiResponse;
+import { mockAuth0WithNoSession } from "../../../../mocks/auth0";
+import { mockRequest, mockResponse } from "../../../../mocks/reqres";
 
 import endpoint from "../../../../src/pages/api/gh/repos";
-import { NextApiRequest, NextApiResponse } from "next";
 import * as E from "fp-ts/lib/Either";
 
+mockAuth0WithNoSession();
+
 test("endpoint returns null without any additional mocking", () =>
-  expect(endpoint(mockRequest, mockResponse).then(E.mapLeft(e => e.type))).resolves.toEqual(
-    E.left("NOT_LOGGED_IN")
-  ));
+  expect(
+    endpoint(mockRequest(), mockResponse()).then(E.mapLeft((e) => e.type))
+  ).resolves.toEqual(E.left("NOT_LOGGED_IN")));
