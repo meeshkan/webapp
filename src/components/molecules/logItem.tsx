21df28ee61b43cab5b104f8612b3771d0ff378d9
Makenna Smutz <makennasmutz@gmail.com>
2020-05-21 11:39:05 +0200

    test log styling finished

--- src/components/molecules/logItem.tsx
+++ src/components/molecules/logItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Icon, Flex, Text, Heading, useColorMode } from "@chakra-ui/core";
+import { Box, Icon, Flex, Text, useColorMode } from "@chakra-ui/core";
 
 type LogProps = {
   method: string;
@@ -10,25 +10,48 @@ type LogProps = {
 const LogItem = ({ method, path, success }: LogProps) => {
   const { colorMode } = useColorMode();
   const cyanBackground = { light: "cyan.50", dark: "cyan.900" };
-  const cyanBorder = { light: "cyan.500", dark: "cyan.300" };
+  const cyanBorder = { light: "cyan.700", dark: "cyan.200" };
   const redBackground = { light: "red.50", dark: "red.900" };
-  const redBorder = { light: "red.500", dark: "red.300" };
+  const redBorder = { light: "red.700", dark: "red.200" };
   return (
-    <Box>
-      <Icon
-        name={success === true ? "check" : "close"}
-        borderColor={
-          success === true ? cyanBorder[colorMode] : redBorder[colorMode]
-        }
-        background={
+    <Box
+      d="flex"
+      alignItems="center"
+      my={3}
+      borderBottom="1px solid"
+      borderColor={`mode.${colorMode}.icon`}
+    >
+      <Box
+        rounded="full"
+        size="32px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mb={4}
+        mr={2}
+        backgroundColor={
           success === true
             ? cyanBackground[colorMode]
             : redBackground[colorMode]
         }
-      />
-      <Flex>
-        <Heading>{method}</Heading>
-        <Text>{path}</Text>
+      >
+        <Icon
+          name={success === true ? "checkmark" : "xmark"}
+          color={
+            success === true ? cyanBorder[colorMode] : redBorder[colorMode]
+          }
+        />
+      </Box>
+      <Flex alignItems="center" mb={4}>
+        <Text
+          fontWeight={600}
+          lineHeight="normal"
+          color={`mode.${colorMode}.text`}
+          mr={1}
+        >
+          {method}
+        </Text>
+        <Text color={`mode.${colorMode}.tertiary`}>{path}</Text>
       </Flex>
     </Box>
   );
