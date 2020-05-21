f12e39ba34763e208642f4cf9622869563dcbf5e
Mike Solomon <mike@meeshkan.com>
2020-05-21 09:50:51 +0300

    Adds additional test for repos

---
+++ test/pages/api/gh/repos/repos.no-token-yet.test.ts
@@ -0,0 +1,14 @@
+import { mockAuth0WithSession } from "../../../../../mocks/auth0";
+import { mockRequest, mockResponse } from "../../../../../mocks/reqres";
+import { mock8BaseWithResult } from "../../../../../mocks/8base";
+
+import endpoint from "../../../../../src/pages/api/gh/repos";
+import * as E from "fp-ts/lib/Either";
+
+mockAuth0WithSession();
+mock8BaseWithResult({ user: { id: "my-id", githubInfo: null } });
+
+test("endpoint returns NO_TOKEN_YET when 8base returns a user that does not have a token", () =>
+  expect(
+    endpoint(mockRequest(), mockResponse()).then(E.mapLeft((e) => e.type))
+  ).resolves.toEqual(E.left("NO_TOKEN_YET")));
