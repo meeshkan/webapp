d2d5e9cb9fdfba9f5a2acf7c20e7765f123e9e5c
Mike Solomon <mike@meeshkan.com>
2020-05-26 18:59:08 +0300

    Adds gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,20 +30,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Meeshkan Webapp</title>
         <link rel="icon" href="/icon.png" />
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-107981669-10"
+        />
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(A,s,a,y,e,r){
-  r=window.asayer=[s,r,e,[y-1]];
-  s=document.createElement('script');s.src=a;s.async=!A;
-  document.getElementsByTagName('head')[0].appendChild(s);
-  r.start=function(v){r.push([0])};
-  r.stop=function(v){r.push([1])};
-  r.userID=function(id){r.push([2,id])};
-  r.userAnonymousID=function(id){r.push([3,id])};
-  r.metadata=function(k,v){r.push([4,k,v])};
-  r.event=function(k,p){r.push([5,k,p])};
-  r.active=function(){return false};
-  r.sessionID=function(){};
-})(0,5331025905788513,'//static.asayer.io/tracker.js',1,29);`,
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107981669-10');`,
           }}
         ></script>
       </Head>
