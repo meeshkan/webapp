19d2e5179841935a7d035b57be262dde3be83821
Mike Solomon <mike@meeshkan.com>
2020-06-09 09:57:02 +0300

    Adds sorting function to tests

--- src/components/Dashboard/production.tsx
+++ src/components/Dashboard/production.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Card from "../molecules/card";
 import { Test } from "../molecules/test";
+import * as A from "fp-ts/lib/Array";
 import { Text, useColorMode } from "@chakra-ui/core";
 import * as t from "io-ts";
+import { newestDateFirst } from "../../utils/date";
+import * as Ord from "fp-ts/lib/Ord";
 
 const TTest = t.type({
   status: t.string,
@@ -18,6 +21,10 @@ type ProductionProps = {
   teamName: string;
   projectName: string;
 };
+export const newestTestFirst: Ord.Ord<ITTest> = {
+  compare: (d0, d1) => newestDateFirst.compare(d0.createdAt, d1.createdAt),
+  equals: (d0, d1) => newestDateFirst.equals(d0.createdAt, d1.createdAt)
+}
 
 const Production = ({ tests, teamName, projectName }: ProductionProps) => {
   const { colorMode } = useColorMode();
@@ -28,19 +35,19 @@ const Production = ({ tests, teamName, projectName }: ProductionProps) => {
           You haven't done any production tests yet.
         </Text>
       ) : (
-        tests.map((test, index) => (
-          <Test
-            key={index}
-            branchName={"master"}
-            commitHash={test.commitHash}
-            date={test.createdAt}
-            status={test.status}
-            testId={test.id}
-            teamName={teamName}
-            projectName={projectName}
-          />
-        ))
-      )}
+          A.sort(newestTestFirst)(tests).map((test, index) => (
+            <Test
+              key={index}
+              branchName={"master"}
+              commitHash={test.commitHash}
+              date={test.createdAt}
+              status={test.status}
+              testId={test.id}
+              teamName={teamName}
+              projectName={projectName}
+            />
+          ))
+        )}
     </Card>
   );
 };
