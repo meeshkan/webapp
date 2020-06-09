ce82ba2a74d163fe215d26ce8266acb8a9751d78
Carolyn Stransky <carolynstransky@gmail.com>
2020-06-09 03:21:31 +0200

    [MEM-575] Remove unused imports, format and add dynamic urls

--- src/components/organisms/navigation.tsx
+++ src/components/organisms/navigation.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import {
   Icon,
-  Box,
   Flex,
   useColorMode,
-  Button,
-  LightMode,
   Stack,
   IconButton,
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator
+  BreadcrumbLink
 } from "@chakra-ui/core";
 import Project from "../molecules/project";
 import Link from "next/link";
-import { Either, isLeft, isRight } from "fp-ts/lib/Either";
+import { Either, isLeft } from "fp-ts/lib/Either";
 import { ISession } from "@auth0/nextjs-auth0/dist/session/session";
 import { Loading } from "../../utils/hookNeedingFetch";
 import { NotAuthorized } from "../../utils/user";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 interface INavigationProps {
   session: Either<Loading, Either<NotAuthorized, ISession>>;
@@ -27,7 +23,7 @@ interface INavigationProps {
 
 const Navigation = ({ session }: INavigationProps) => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const router = useRouter()
+  const router = useRouter();
   const pathForBreadcrumbs = router.asPath.split("/").slice(1);
 
   return (
@@ -59,23 +55,27 @@ const Navigation = ({ session }: INavigationProps) => {
           <Breadcrumb ml={3} mt={2} addSeparator={true}>
             {pathForBreadcrumbs.map((crumb, i, pathForBreadcrumbs) => {
               if (pathForBreadcrumbs.length - 1 === i) {
-                return(
+                return (
                   <BreadcrumbItem key={i} isCurrentPage>
                     <BreadcrumbLink color={`mode.${colorMode}.tertiary`}>
                       {crumb}
                     </BreadcrumbLink>
                   </BreadcrumbItem>
-                )
+                );
               } else {
-                return(
+                const url = pathForBreadcrumbs.slice(0, i + 1).join("/");
+                return (
                   <BreadcrumbItem key={i}>
-                    <BreadcrumbLink href={`/${crumb}`} color={`mode.${colorMode}.tertiary`}>
+                    <BreadcrumbLink
+                      href={`/${url}`}
+                      color={`mode.${colorMode}.tertiary`}
+                    >
                       {crumb}
                     </BreadcrumbLink>
                   </BreadcrumbItem>
-                )
+                );
               }
-              })}
+            })}
           </Breadcrumb>
         </Flex>
         {!isLeft(session) && (
