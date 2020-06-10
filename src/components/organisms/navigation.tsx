d8b7d010c71ef0c9a9259ad7aa22ee08b0ac358c
Carolyn <carolyn@meeshkan.com>
2020-06-10 02:09:09 +0200

    [MEM-575] Add breadcrumbs to navigation (#76)
    * [MEM-575] Add stubbed breadcrumbs and initial CSS
    
    * [MEM-575] Use pathname for crumbs
    
    * [MEM-575] Add current page functionality
    
    * [MEM-575] Add link href
    
    * [MEM-575] Remove unused imports, format and add dynamic urls
    
    * Apply suggestions from Makenna's review
    
    Co-authored-by: Makenna Smutz <makenna@meeshkan.com>
    
    Co-authored-by: Makenna Smutz <makenna@meeshkan.com>

--- src/components/organisms/navigation.tsx
+++ src/components/organisms/navigation.tsx
@@ -3,17 +3,19 @@ import {
   Icon,
   Flex,
   useColorMode,
-  Button,
-  LightMode,
   Stack,
   IconButton,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink
 } from "@chakra-ui/core";
 import Project from "../molecules/project";
 import Link from "next/link";
-import { Either, isLeft, isRight } from "fp-ts/lib/Either";
+import { Either, isLeft } from "fp-ts/lib/Either";
 import { ISession } from "@auth0/nextjs-auth0/dist/session/session";
 import { Loading } from "../../utils/hookNeedingFetch";
 import { NotAuthorized } from "../../utils/user";
+import { useRouter } from "next/router";
 
 interface INavigationProps {
   session: Either<Loading, Either<NotAuthorized, ISession>>;
@@ -21,6 +23,8 @@ interface INavigationProps {
 
 const Navigation = ({ session }: INavigationProps) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const router = useRouter();
+  const pathForBreadcrumbs = router.asPath.split("/").slice(1);
 
   return (
     <>
@@ -34,15 +38,46 @@ const Navigation = ({ session }: INavigationProps) => {
         pos="sticky"
         top={8}
       >
-        <Link href="/">
-          <Icon
-            name="Logo"
-            color={`mode.${colorMode}.title`}
-            h={8}
-            w="auto"
-            cursor="pointer"
-          />
-        </Link>
+        <Flex
+          align="center"
+          backgroundColor={`mode.${colorMode}.card`}
+          rounded="sm"
+        >
+          <Link href="/">
+            <Icon
+              name="Logo"
+              color={`mode.${colorMode}.title`}
+              h={8}
+              w="auto"
+              cursor="pointer"
+            />
+          </Link>
+          <Breadcrumb ml={3} mt={2} addSeparator={true} color={`mode.${colorMode}.text`}>
+            {pathForBreadcrumbs.map((crumb, i, pathForBreadcrumbs) => {
+              if (pathForBreadcrumbs.length - 1 === i) {
+                return (
+                  <BreadcrumbItem key={i} isCurrentPage>
+                    <BreadcrumbLink color={`mode.${colorMode}.tertiary`}>
+                      {crumb}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                );
+              } else {
+                const url = pathForBreadcrumbs.slice(0, i + 1).join("/");
+                return (
+                  <BreadcrumbItem key={i}>
+                    <BreadcrumbLink
+                      href={`/${url}`}
+                      color={`mode.${colorMode}.tertiary`}
+                    >
+                      {crumb}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                );
+              }
+            })}
+          </Breadcrumb>
+        </Flex>
         {!isLeft(session) && (
           <section>
             {isLeft(session.right) ? (
