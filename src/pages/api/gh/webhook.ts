683d22b5edc769f1d752d1ef8729605b801483d1
Mike Solomon <mike@meeshkan.com>
2020-06-11 15:25:55 +0300

    Revert "purrrggggeeeee"
    This reverts commit 7b21867ee9805f9eb4eb1c314bfe88ad437a466e.

---
+++ src/pages/api/gh/webhook.ts
@@ -0,0 +1,100 @@
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as t from "io-ts";
+import {
+  METHOD_NOT_POST,
+  UNDEFINED_ERROR,
+  INVALID_SECRET_FROM_GITHUB,
+  REST_ENDPOINT_ERROR,
+  FILTERED_OUT,
+} from "../../../utils/error";
+import safeApi, { _400ErrorHandler } from "../../../utils/safeApi";
+import crypto from "crypto";
+import { constNull, constVoid, flow } from "fp-ts/lib/function";
+import fetch from "isomorphic-unfetch";
+
+type NegativeWebhookOutcome =
+  | METHOD_NOT_POST
+  | INVALID_SECRET_FROM_GITHUB
+  | FILTERED_OUT
+  | REST_ENDPOINT_ERROR
+  | UNDEFINED_ERROR;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default safeApi(
+  (req, res) =>
+    pipe(
+      req.method === "POST"
+        ? TE.right(constVoid())
+        : TE.left({ type: "METHOD_NOT_POST" }),
+      TE.chain<NegativeWebhookOutcome, void, string>((_) => () =>
+        new Promise((resolve, _) => {
+          let data = "";
+          req.on("data", (chunk) => {
+            data += typeof chunk === "string" ? chunk : chunk.toString();
+          });
+          req.on("end", () => resolve(E.right(data)));
+        })
+      ),
+      TE.chain<NegativeWebhookOutcome, string, string>((body) =>
+        "sha1=" +
+          crypto
+            .createHmac("sha1", process.env.GH_WEBHOOK_SECRET)
+            .update(body)
+            .digest("hex") ===
+        req.headers["x-hub-signature"]
+          ? TE.right(body)
+          : TE.left({
+              type: "INVALID_SECRET_FROM_GITHUB",
+              msg:
+                "Could not decode secret from github with signature " +
+                req.headers["x-hub-signature"] +
+                req.rawHeaders.join(" :: "),
+            })
+      ),
+      TE.chain(flow(JSON.parse, TE.right)),
+      TE.chain<NegativeWebhookOutcome, any, any>((body) =>
+        body.installation
+          ? TE.right(body)
+          : TE.left({
+              type: "FILTERED_OUT",
+              msg: "Did not receive an installation event",
+            })
+      ),
+      TE.chain((body) =>
+        TE.tryCatch(
+          () =>
+            fetch(process.env.SLACK_GH_WEBHOOK, {
+              method: "post",
+              body: JSON.stringify({
+                text: JSON.stringify(body, null, 2),
+              }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }),
+          (error): NegativeWebhookOutcome => ({
+            type: "UNDEFINED_ERROR",
+            msg: "Call to slack webhook for github app failed",
+            error,
+          })
+        )
+      ),
+      TE.chain((res) =>
+        res.ok
+          ? TE.right(constNull())
+          : TE.left<NegativeWebhookOutcome, void>({
+              type: "REST_ENDPOINT_ERROR",
+              msg: "Call to slack webhook failed",
+            })
+      ),
+      TE.chain((_) => TE.right(res.status(200)))
+    ),
+  _400ErrorHandler
+);
---
+++ src/pages/api/slack/oauth.ts
@@ -0,0 +1,166 @@
+import { ISession } from "@auth0/nextjs-auth0/dist/session/session";
+import crypto from "crypto";
+import * as A from "fp-ts/lib/Array";
+import * as E from "fp-ts/lib/Either";
+import { constant, constVoid, flow } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/pipeable";
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as t from "io-ts";
+import fetch from "isomorphic-unfetch";
+import {
+  UPDATE_SLACK_INFO_MUTATION,
+  CREATE_SLACK_INFO_MUTATION,
+} from "../../../gql/pages/api/slack/oauth";
+import {
+  defaultGQLErrorHandler,
+  ID_NOT_IN_STATE,
+  INCORRECT_TYPE_SAFETY,
+  INVALID_TOKEN_ERROR,
+  NOT_LOGGED_IN,
+  UNDEFINED_ERROR,
+  UNKNOWN_GRAPHQL_ERROR,
+} from "../../../utils/error";
+import { eightBaseClient, upsertHack } from "../../../utils/graphql";
+import * as Oauth from "../../../utils/oauth";
+import safeApi, { _400ErrorHandler } from "../../../utils/safeApi";
+import { encrypt } from "../../../utils/sec";
+import { SEPARATOR } from "../../../utils/separator";
+import { confirmOrCreateUser, getUserIdFromIdOrEnv } from "../../../utils/user";
+import { withSession } from "../session";
+
+const userType = t.type({ id: t.string });
+
+const slackTokenType = t.type({
+  access_token: t.string,
+  scope: t.string,
+  team: t.type({ name: t.string, id: t.string }),
+  incoming_webhook: t.type({
+    url: t.string,
+    channel: t.string,
+    configuration_url: t.string,
+  }),
+  bot_user_id: t.string,
+});
+
+type SlackTokenType = t.TypeOf<typeof slackTokenType>;
+
+const authenticateAppWithSlack = (
+  userId: string,
+  teamName: string,
+  projectName: string,
+  params: URLSearchParams
+) => (session: ISession): TE.TaskEither<NegativeOAuthOutcome, void> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        fetch(process.env.SLACK_OAUTH_APP_URL, {
+          method: "post",
+          body: params,
+        }),
+      (error): NegativeOAuthOutcome => ({
+        type: "UNDEFINED_ERROR",
+        msg: "Call to slack failed",
+        error,
+      })
+    ),
+    TE.chain((res) =>
+      res.ok
+        ? TE.tryCatch(
+            () => res.json(),
+            (error) => ({
+              type: "UNDEFINED_ERROR",
+              msg: "Conversion of text response from slack failed",
+              error,
+            })
+          )
+        : TE.left({
+            type: "REST_ENDPOINT_ERROR",
+            msg: `Could not call slack endpoint: ${res.status} ${res.statusText}`,
+          })
+    ),
+    TE.chainEitherK<NegativeOAuthOutcome, any, SlackTokenType>(
+      flow(
+        slackTokenType.decode,
+        E.mapLeft(
+          (errors): NegativeOAuthOutcome => ({
+            type: "INCORRECT_TYPE_SAFETY",
+            msg: "Could not decode the slack token information",
+            errors,
+          })
+        )
+      )
+    ),
+    TE.chain((slackToken) =>
+      TE.right(
+        encrypt(
+          JSON.stringify(slackToken),
+          crypto.randomBytes(16),
+          process.env.SLACK_TOKEN_SIGNING_KEY
+        )
+      )
+    ),
+    TE.chain(({ encryptedData, iv }) =>
+      TE.tryCatch(
+        () =>
+          upsertHack(
+            session,
+            CREATE_SLACK_INFO_MUTATION,
+            UPDATE_SLACK_INFO_MUTATION,
+            {
+              userId: getUserIdFromIdOrEnv(userId),
+              slackSyncCheckSum: encryptedData,
+              slackSyncNonce: iv,
+              teamName,
+              namePlusTeamName: projectName + SEPARATOR + teamName,
+            }
+          ).then(constVoid),
+        defaultGQLErrorHandler("insert slack token mutation")
+      )
+    )
+  );
+
+type NegativeOAuthOutcome =
+  | ID_NOT_IN_STATE
+  | INCORRECT_TYPE_SAFETY
+  | UNDEFINED_ERROR
+  | INVALID_TOKEN_ERROR
+  | UNKNOWN_GRAPHQL_ERROR
+  | NOT_LOGGED_IN;
+
+export const fromQueryParam = (p: string | string[]) =>
+  t.string.is(p) ? p : pipe(A.head(p), O.getOrElse(constant("")));
+
+export default safeApi<NegativeOAuthOutcome, void>(
+  (req, res) =>
+    pipe(
+      Oauth.Oauth(
+        req,
+        process.env.SLACK_OAUTH_FLOW_SIGNING_KEY,
+        t.type({ id: t.string, teamName: t.string, projectName: t.string })
+      ),
+      RTE.chain(({ teamName, projectName }) =>
+        pipe(
+          confirmOrCreateUser("id", userType),
+          RTE.chain(({ id }) => RTE.right({ id, teamName, projectName }))
+        )
+      ),
+      RTE.chain(({ id, teamName, projectName }) =>
+        authenticateAppWithSlack(
+          id,
+          teamName,
+          projectName,
+          new URLSearchParams({
+            code: fromQueryParam(req.query.code),
+            client_id: process.env.SLACK_OAUTH_APP_CLIENT_ID,
+            client_secret: process.env.SLACK_OAUTH_APP_CLIENT_SECRET,
+            redirect_uri: process.env.SLACK_OAUTH_REDIRECT_URI,
+          })
+        )
+      ),
+      RTE.chain((_) => Oauth.Redirect(res)),
+      withSession(req, "oauth.ts default export")
+    ),
+  _400ErrorHandler
+);
