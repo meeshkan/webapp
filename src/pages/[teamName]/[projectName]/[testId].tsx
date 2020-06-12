f43c1bedb298ae7d385018e0cf446e3480cafb64
Mike Solomon <mike@meeshkan.com>
2020-06-12 08:17:08 +0300

    Adds versioning to test log

--- src/pages/[teamName]/[projectName]/[testId].tsx
+++ src/pages/[teamName]/[projectName]/[testId].tsx
@@ -23,6 +23,7 @@ import FailureMessage from "../../../components/molecules/failureMessage";
 import LogItem from "../../../components/molecules/logItem";
 import * as _E from "../../../fp-ts/Either";
 import { LensTaskEither, lensTaskEitherHead } from "../../../monocle-ts";
+import { versionTriage } from "../../../utils/testLog";
 import { withSession } from "../../../pages/api/session";
 import {
   defaultGQLErrorHandler,
@@ -204,7 +205,7 @@ const TestPage = withError<GET_SERVER_SIDE_PROPS_ERROR, ITestProps>(
       pipe(
         {
           colorMode: useColorMode().colorMode,
-          logs: JSON.parse(log),
+          logs: versionTriage(JSON.parse(log)),
         },
         (p) => ({
           ...p,
