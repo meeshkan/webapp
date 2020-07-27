463e60bb38ec8dd383998d98ab9a09b2a6fd7052
Makenna Smutz <makennasmutz@gmail.com>
2020-07-27 12:45:06 +0200

    Set up initial new sign up / log in screen

--- src/components/organisms/signIn.tsx
+++ src/components/organisms/signIn.tsx
@@ -2,60 +2,96 @@ import React from "react";
 import {
   Box,
   Heading,
-  Flex,
+  Stack,
   LightMode,
   Button,
   Text,
   useColorMode,
   Link,
+  SimpleGrid,
 } from "@chakra-ui/core";
 import { useRouter } from "next/router";
+import Card from "../molecules/card";
 
 const SignIn = () => {
   const { colorMode } = useColorMode();
   const router = useRouter();
 
   return (
-    <Box
-      as="section"
-      mt={24}
-      mx="auto"
-      p={8}
-      backgroundColor={`mode.${colorMode}.card`}
-      maxW="750px"
-      borderRadius="sm"
-    >
-      <Heading
-        as="h2"
-        color={`mode.${colorMode}.title`}
-        textAlign="center"
-        mb={6}
-      >
-        Sign in to start using Meeshkan
-      </Heading>
-      <Text
-        textAlign="center"
-        mb={16}
-        lineHeight="tall"
-        fontSize="xl"
-        fontStyle="italic"
-        fontWeight={500}
-      >
-        Meeshkan is currently in alpha and by invitation only.
-        <br />
-        <Link href="https://meeshkan.com/" isExternal>
-          Request alpha access
-        </Link>{" "}
-        if you're interested.
-      </Text>
-      <Flex justify="center">
-        <LightMode>
-          <Button colorScheme="red" onClick={() => router.push("/api/login")}>
-            Sign in
-          </Button>
-        </LightMode>
-      </Flex>
-    </Box>
+    <SimpleGrid columns={2} gap={8} h="80vh">
+      <Card></Card>
+
+      <Card>
+        <Heading
+          as="h2"
+          color={`mode.${colorMode}.title`}
+          textAlign="center"
+          my={8}
+        >
+          Log in or Sign up
+        </Heading>
+        <Text
+          textAlign="center"
+          mb={16}
+          lineHeight="tall"
+          fontSize="xl"
+          fontStyle="italic"
+          fontWeight={500}
+        >
+          Meeshkan is an automated GraphQL server testing tool.
+          <br />
+          <Link href="https://meeshkan.com/" isExternal>
+            See here for more information about Meeshkan.
+          </Link>
+        </Text>
+        <Stack
+          direction="column"
+          justify="center"
+          spacing={16}
+          maxW="400px"
+          mx="auto"
+        >
+          <Stack direction="column" justify="center">
+            <Text
+              textAlign="center"
+              mb={4}
+              lineHeight="tall"
+              fontSize="xl"
+              fontStyle="italic"
+              fontWeight={500}
+            >
+              Have an account already?
+            </Text>
+            <Button
+              colorScheme="gray"
+              onClick={() => router.push("/api/login")}
+            >
+              Log in
+            </Button>
+          </Stack>
+          <Stack direction="column" justify="center">
+            <Text
+              textAlign="center"
+              mb={4}
+              lineHeight="tall"
+              fontSize="xl"
+              fontStyle="italic"
+              fontWeight={500}
+            >
+              New to Meeshkan?
+            </Text>
+            <LightMode>
+              <Button
+                colorScheme="red"
+                onClick={() => router.push("/api/login")}
+              >
+                Sign up
+              </Button>
+            </LightMode>
+          </Stack>
+        </Stack>
+      </Card>
+    </SimpleGrid>
   );
 };
 
